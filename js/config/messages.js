ca532a86a48803b0a8288650f185285aea56794f
satyamsingh92 <satyamsingh1991@gmail.com>
2018-04-07 01:40:41 +0530

    assignment ui

---
+++ js/config/messages.js
@@ -0,0 +1,20 @@
+messages = {
+    "labels": {
+        "person": " PERSON",
+        "employeeForm": "Employee Form",
+        "firstname": "First Name:",
+        "lastname": "Last Name:",
+        "email": "Email:",
+        "title": "Title:",
+        "role": "Role:",
+        "socialAccountsURL": "Social accounts URL:",
+        "isPrimaryCompany":"Is primary company?",
+        "addPerson":"Add Person",
+        "close":"Close",
+        "listOfEmployees":"EMPLOYEES_LiST",
+        "name":"Name",
+        "socialLinks":"Social Links",
+        "organisationStructure":" HIRARCHI STRUCTURE",
+        "userAdded":'User Added Successfully...'
+    }
+};
